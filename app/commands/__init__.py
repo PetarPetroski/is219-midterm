730e32d4e73cde7d46221af0fb515d2d1e53b1b7
Petar Petroski <petar.petroski2003@gmail.com>
2024-03-28 00:38:39 -0400

    implemented history command, clear command, save command, and load command

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from abc import ABC, abstractmethod
 
 class Command(ABC):
@@ -7,6 +8,7 @@ class Command(ABC):
 class CommandHandler:
     def __init__(self):
         self.commands = {}
+        self.history = pd.DataFrame(columns=['command', 'args', 'result'])
 
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
@@ -16,8 +18,18 @@ class CommandHandler:
         args = ' '.join(args)
         if command_name == "menu":
             self.list_commands()
+        elif command_name == "history":
+            self.show_history()
+        elif command_name == "save":
+            self.save_history()
+        elif command_name == "load":
+            self.load_history()
+        elif command_name == "clear":
+            self.clear_history()
         elif command_name in self.commands:
-            self.commands[command_name].execute(args)
+            result = self.commands[command_name].execute(args)
+            new_row = pd.DataFrame({'command': [command_name], 'args': [args], 'result': [result]})
+            self.history = self.history._append(new_row, ignore_index=True)        
         else:
             print(f"No such command: {command_name}")
 
@@ -25,3 +37,15 @@ class CommandHandler:
         print("Available commands:")
         for command_name in self.commands:
             print(command_name)
+
+    def show_history(self):
+        print(self.history)
+
+    def save_history(self):
+        self.history.to_csv('history.csv', index=False)
+
+    def load_history(self):
+        self.history = pd.read_csv('history.csv')
+
+    def clear_history(self):
+        self.history = pd.DataFrame(columns=['command', 'args', 'result'])
