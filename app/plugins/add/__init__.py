d8fd26c1580f943dd8620d239ef2f375921c78eb
Petar Petroski <petar.petroski2003@gmail.com>
2024-03-29 11:40:36 -0400

    ensured error checking that required at least two numbers to be able to add

--- app/plugins/add/__init__.py
+++ app/plugins/add/__init__.py
@@ -4,6 +4,9 @@ import logging
 class Add(Command):
     def execute(self, args):
         numbers = args.split()
+        if len(numbers) < 2:
+            logging.error("At least 2 numbers are required.")
+            return None
         result = sum(map(int, numbers))
         logging.info(f"The sum is: {result}")
         return result
