f27dc3d8ee868f735a9c26cc6366a50e96e2ec47
Petar Petroski <petar.petroski2003@gmail.com>
2024-03-29 11:35:55 -0400

    adjusted logging

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import logging
 from abc import ABC, abstractmethod
+
 import logging
 import logging.config
 
@@ -12,7 +13,7 @@ class Command(ABC):
 class CommandHandler:
     def __init__(self):
         self.commands = {}
-        self.history = pd.DataFrame(columns=['command', 'args'])
+        self.history = pd.DataFrame(columns=['command', 'args', 'result'])
 
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
@@ -25,19 +26,15 @@ class CommandHandler:
             logging.info("Menu command executed")
         elif command_name == "history":
             self.show_history()
-            logging.info("History command executed")
         elif command_name == "save":
             self.save_history()
-            logging.info("Save command executed")
         elif command_name == "load":
             self.load_history()
-            logging.info("Load command executed")
         elif command_name == "clear":
             self.clear_history()
-            logging.info("Clear command executed")
         elif command_name in self.commands:
             result = self.commands[command_name].execute(args)
-            new_row = pd.DataFrame({'command': [command_name], 'args': [args]})
+            new_row = pd.DataFrame({'command': [command_name], 'args': [args], 'result': [result]})
             self.history = self.history._append(new_row, ignore_index=True)
             logging.info(f"Command '{command_name}' executed with args '{args}'")
         else:
@@ -50,14 +47,10 @@ class CommandHandler:
             print(command_name)
 
     def show_history(self):
-        for index, row in self.history.iterrows():
-            command = row['command']
-            args = row['args']
-            if command in self.commands:
-                result = self.commands[command].execute(args)
-                print(f"Command: {command}, Args: {args}, Result: {result}")
-            else:
-                print(f"Command: {command}, Args: {args}, Result: Command not found")
+        if self.history.empty:
+            print("No history found")
+        else:
+            print(self.history.to_string(index=False))
         logging.info("History displayed")
 
     def save_history(self):
@@ -69,5 +62,5 @@ class CommandHandler:
         logging.info("History loaded from file")
 
     def clear_history(self):
-        self.history = pd.DataFrame(columns=['command', 'args'])
+        self.history = pd.DataFrame(columns=['command', 'args', 'result'])
         logging.info("History cleared")
